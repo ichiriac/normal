f2f0ce044a7855646b2d5cac7f9448aff8cc778f
Ioan CHIRIAC <i.chiriac@yahoo.com>
2015-01-11 12:33:46 +0100

    update the model structure + tests

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /**
  * Main api
  */
-
-module.exports.Model = require('./lib/model');
-module.exports.Entity = require('./lib/record');
 module.exports.Session = require('./lib/session');
+module.exports.Model = require('./lib/model');
+module.exports.Entity = require('./lib/entity');
 module.exports.create = function(db, config) {
   return new this.Session(db, config, this.Model, this.Entity);
 };
--- test/simple.js
+++ test/simple.js
@@ -1,7 +1,35 @@
 var assert = require("assert");
 var lib = require("../index");
 
-describe('Simple Connect', function() {
-  var session = lib.create(null, {
+describe('simple tests', function() {
+
+  it('lib api', function(){
+    assert(typeof lib.create === 'function');
+    assert(typeof lib.Session === 'function');
+    assert(typeof lib.Model === 'function');
+    assert(typeof lib.Entity === 'function');
+  });
+  it('session api', function(){
+    var session = lib.create(null, {
+      users: {
+        table: 'users',
+        properties: {
+          'id': 'primary',
+          'name': 'string(60)',
+          'email': 'string(250)',
+          'pwd': {
+            column: 'password',
+            type: 'string(32)'
+          }
+        },
+        relations: {
+          
+        }
+      }
+    });
+    assert(session.db === null);
+    assert(session instanceof lib.Session);
+    assert(session.baseModel === lib.Model);
+    assert(session.baseEntity === lib.Entity);
   });
 });
