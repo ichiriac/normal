4d4e403044aba153d0d47db08344b462d035b5ad
Ioan CHIRIAC <173203+ichiriac@users.noreply.github.com>
2025-11-09 22:01:26 +0000

    remove index.js file as part of codebase migration to TypeScript

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { Field } = require('./src/fields/Base');
-const { Discovery } = require('./src/cache/Discovery');
-module.exports = {
-  ...require('./src/Connection'),
-  ...require('./src/Repository'),
-  Fields: Field,
-  Discovery,
-};
